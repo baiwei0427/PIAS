0eadbe043b862ddbf8078adb5ef17575cc78b94a
wei bai <baiwei0427@sina.com>
2015-03-20 23:49:59 +0800

    Update

--- sch_pias/sch_pias.c
+++ sch_pias/sch_pias.c
@@ -5,10 +5,15 @@
 #include <linux/pkt_sched.h>
 #include <net/sch_generic.h>
 #include <net/pkt_sched.h>
+#include <linux/ip.h>
+#include <net/dsfield.h>
+#include <net/inet_ecn.h>
 
 #define PIAS_MAX_PRIO 2
 
-#define PIAS_MAX_LEN_BYTES 1048576 //1MB should be enough
+#define PIAS_MAX_LEN_BYTES 1048576	//1MB should be enough
+
+#define PIAS_ECN_THRESH_BYTES 30720	//Per-port ECN marking threshold 
 
 #define PIAS_RATE_BPS 980000000
 
@@ -19,27 +24,130 @@
 struct pias_sched_data 
 {
 /* Parameters */
-	struct Qdisc **queues; /* Priority queues */
+	struct Qdisc **queues; /* Priority queues where queues[0] has the highest priority*/
 	u64 rate_bps;	/* Rate in bps */
 
 /* Variables */
 	u64 tokens;	/* Tokens in bytes */ 
-	u32 max_len; 	/* Maximum length of all queues in bytes */
-	spinlock_t spinlock;
+	u32 sum_len;	/* The sum of lengh og all queues in bytes */
+	u32 max_len; 	/* Maximum value of sum_len*/
+	//spinlock_t spinlock;
 	struct Qdisc *sch; 
 	s64	time_us;	/* Time check-point */ 
 	struct hrtimer timer;	/* Watchdog timer */
 }
 
-/*
- * Borrow somethings from skb_gso_mac_seglen in sch_tbf.c 
- * We use this function to account for the true number of bytes sent on wire.
- */
-static inline int skb_size(struct sk_buff *skb)
+/* We use this function to account for the true number of bytes sent on wire.*/
+static inline unsigned int skb_size(struct sk_buff *skb)
 {
 	return max(skb->len,PIAS_MIN_PACKET_BYTES);
 }
 
+static inline void pias_qdisc_ecn(struct sk_buff *skb)
+{
+	if(skb_make_writable(skb,sizeof(struct iphdr))&&ip_hdr(skb))
+		IP_ECN_set_ce(ip_hdr(skb));
+}
+
+static struct Qdisc * pias_qdisc_classify(struct sk_buff *skb, struct Qdisc *sch)
+{
+	struct pias_sched_data *q = qdisc_priv(sch);	
+	int i;
+	
+	if(q->queues==NULL)
+		return NULL;
+	
+	/* Return the highest priority queue */
+	for(i=0;i<PIAS_MAX_PRIO && q->queues[i]; i++)
+		return q->queues[i];
+	
+	return NULL;
+}
+
+/* Start watchdog timer when Qdisc is throttled */
+static inline void pias_qdisc_watchdog(struct pias_sched_data *q, u64 dt_ns)
+{
+	if(!hrtimer_active(&q->timer)) 
+	{
+		hrtimer_start(&q->timer, ktime_set(0, dt_ns),HRTIMER_MODE_REL);
+	}
+}
+
+enum hrtimer_restart pias_qdisc_timeout(struct hrtimer *timer) 
+{
+	struct pias_sched_data *q = container_of(timer, struct pias_sched_data, timer);
+	qdisc_unthrottled(q->sch);
+	return HRTIMER_NORESTART;
+}
+
+static int pias_qdisc_enqueue(struct sk_buff *skb, struct Qdisc *sch)
+{
+	struct Qdisc *qdisc;
+	unsigned int len=skb_size(skb);
+	int ret;
+	struct pias_sched_data *q = qdisc_priv(sch);	
+	
+	qdisc=pias_qdisc_classify(skb,sch);
+	
+	//No appropriate priority queue or queue limit is reached
+	if(qdisc==NULL||sum_len+len>q->max_len)
+	{
+		qdisc_qstats_drop(sch);
+		//kfree_skb(skb);
+		return NET_XMIT_DROP;
+	}
+	else
+	{
+		//ECN marking
+		if(sum_len+len>PIAS_ECN_THRESH_BYTES)
+			pias_qdisc_ecn(skb);
+		
+		ret=qdisc_enqueue(skb, qdisc);
+		if(ret == NET_XMIT_SUCCESS) 
+		{
+			sch->q.qlen++;
+			sum_len+=len;
+			return NET_XMIT_SUCCESS;
+		}
+		else
+		{
+			if (net_xmit_drop_count(ret))
+				qdisc_qstats_drop(sch);
+			return ret;
+		}
+	}
+}
+
+/* We don't need this */
+static unsigned int pias_qdisc_drop(struct Qdisc *sch)
+{
+	return 0;
+}
+
+/* We don't use TC netlink interfaces */
+static int pias_qdisc_change(struct Qdisc *sch, struct nlattr *opt)
+{
+	return 0;	
+}
+
+/* Release Qdisc resources */
+static void pias_qdisc_destroy(struct Qdisc *sch)
+{
+	struct pias_sched_data *q = qdisc_priv(sch);
+	int i;
+
+	if(q->queues!=NULL)
+	{
+		for(i = 0; i < PIAS_MAX_PRIO && q->queues[i]; i++)
+			qdisc_destroy(q->queues[i]);
+
+		kfree(q->queues);
+		q->queues = NULL;
+	}
+	hrtimer_cancel(&q->timer);
+}
+
+/* Initialize Qdisc */
 static int pias_qdisc_init(struct Qdisc *sch, struct nlattr *opt)
 {
 	int i;
@@ -53,9 +161,10 @@ static int pias_qdisc_init(struct Qdisc *sch, struct nlattr *opt)
 	if(q->queuess == NULL)
 		return -ENOMEM;
 	
-	spin_lock_init(&q->spinlock);
+	//spin_lock_init(&q->spinlock);
 	q->time_us = ktime_to_ns(ktime_get());
 	q->rate_bps = PIAS_RATE_BPS;
+	q->sum_len=0;
 	q->max_len = PIAS_MAX_LEN_BYTES;	
 	q->sch = sch;
 	hrtimer_init(&q->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
@@ -88,7 +197,6 @@ static struct Qdisc_ops pias_qdisc_ops __read_mostly = {
 	.peek = pias_qdisc_peek,
 	.drop = pias_qdisc_drop,
 	.change = pias_qdisc_change,
-	.attach = pias_qdisc_attach,
 	.dump = pias_qdisc_dump,
 	.owner = THIS_MODULE,
 };
